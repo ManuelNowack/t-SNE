39262d955dfa2b0ce5a7e061540a90a9f1fe91e9
Gabriel Fringeli <fgabriel@student.ethz.ch>
2021-05-04 13:47:32 +0200

    Incorporate benchmark_full into benchmark

--- README.md
+++ README.md
@@ -7,13 +7,9 @@ cmake --build build
 
 To run the benchmark:
 ```shell
-build/bin/benchmark <path_to_X_PCA> <path_to_Y_INIT>
+build/bin/benchmark <path_to_X_PCA> <path_to_Y_INIT> [<log2_min_samples> <log2_max_samples>] 
 ```
-
-To run the benchmark for different numbers of samples
-```shell
-build/bin/benchmark_full <path_to_X_PCA> <path_to_Y_INIT> <log2_of_min_samples> <log2_of_max_samples>
-```
+If `<log2_min_samples>` and `<log2_max_samples>` are not defined, run benchmark for full dataset only. Otherwise, run benchmarks for `2^<log2_min_samples>` up to and including `2^<log2_min_samples>` in multiplicative steps of $2$.
 
 To enable debug output:
 ```shell
--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -41,13 +41,49 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  if (argc < 3) {
-    cerr << "Usage: " << argv[0] << " X_PCA Y_INIT" << endl;
+  if (argc != 3 && argc != 5) {
+    cerr << "Usage: " << argv[0] << " X_PCA Y_INIT [MIN MAX]" << endl;
+    cerr << "If MIN and MAX are not defined, benchmark for full dataset." << endl;
+    cerr << "Otherwise, benchmark from 2^MIN up to 2^MAX samples with multiplicative steps of 2." << endl;
     return 1;
   }
 
   Matrix X = load_matrix(argv[1]);
   Matrix Y = load_matrix(argv[2]);
+
+  // Determine number of samples used for benchmarking
+  int n_measurements;
+  int log2_min_samples, log2_max_samples;
+  if (argc == 3) {
+    // All samples only
+    n_measurements = 1;
+  } else {
+    // Different numbers of samples
+    log2_min_samples = atoi(argv[3]);
+    log2_max_samples = atoi(argv[4]);
+    if (log2_min_samples > log2_max_samples || log2_min_samples < 0 || log2_max_samples < 0) {
+      cerr << "Invalid sample bounds: MIN=" << log2_min_samples << ", MAX=" << log2_max_samples << endl;
+      return 1;
+    }
+    if (X.nrows < powl(2, log2_max_samples)) {
+      cerr << "Maximum number of samples (2^" << log2_max_samples
+      << " = " << (int)powl(2, log2_max_samples)
+      << ") is higher than the number of samples in the dataset provided ("
+      << X.nrows << ")" << endl;
+      return 1;
+    }
+    n_measurements = log2_max_samples - log2_min_samples + 1;
+  }
+
+  // Create array of sample numbers
+  int n_samples_values[n_measurements];
+  if (argc == 3) {
+    n_samples_values[0] = X.nrows;
+  } else {
+    for (int i=0; i<n_measurements; i++) {
+      n_samples_values[i] = (int)powl(2, log2_min_samples+i);
+    }
+  }
 
   register_functions();
   auto &tsne_func_registry = FuncResitry<tsne_func_t>::get_instance();
@@ -57,23 +93,70 @@ int main(int argc, char **argv) {
 
   // TODO(mrettenba): Check validity of functions.
 
-  double perf;
+  int n_measurement_series = tsne_func_registry.num_funcs + joint_probs_func_registry.num_funcs + grad_desc_func_registry.num_funcs;
+  double performances[n_measurements][n_measurement_series];
+
+  for (int i_measurement=0; i_measurement<n_measurements; i_measurement++) {
+    cout << n_samples_values[i_measurement] << " samples" << endl;
+
+    int n_samples = n_samples_values[i_measurement];
+    Matrix X_sub = {.nrows = n_samples, .ncols = X.ncols, .data = X.data};
+    Matrix Y_sub = {.nrows = n_samples, .ncols = Y.ncols, .data = Y.data};
+
+    int i_series = 0;
+
+    double perf;
+    for (int i = 0; i < tsne_func_registry.num_funcs; i++) {
+      perf = perf_test_tsne(tsne_func_registry.funcs[i], X_sub, Y_sub);
+      cout << tsne_func_registry.func_names[i] << "," << perf << endl;
+      performances[i_measurement][i_series] = perf;
+      i_series++;
+    }
+
+    for (int i = 0; i < joint_probs_func_registry.num_funcs; i++) {
+      perf = perf_test_joint_probs(joint_probs_func_registry.funcs[i], X_sub);
+      cout << joint_probs_func_registry.func_names[i] << "," << perf << endl;
+      performances[i_measurement][i_series] = perf;
+      i_series++;
+    }
+
+    // Pick one joint_probs implementation to populate the variables.
+    auto joint_probs = joint_probs_func_registry.funcs[0];
+    for (int i = 0; i < grad_desc_func_registry.num_funcs; i++) {
+      perf = perf_test_grad_desc(grad_desc_func_registry.funcs[i], joint_probs, X_sub,
+                                 Y_sub);
+      cout << grad_desc_func_registry.func_names[i] << "," << perf << endl;
+      performances[i_measurement][i_series] = perf;
+      i_series++;
+    }
+    
+    cout << endl;
+  }
+
   for (int i = 0; i < tsne_func_registry.num_funcs; i++) {
-    perf = perf_test_tsne(tsne_func_registry.funcs[i], X, Y);
-    cout << tsne_func_registry.func_names[i] << "," << perf << endl;
+    cout << tsne_func_registry.func_names[i] << ", ";
   }
-
   for (int i = 0; i < joint_probs_func_registry.num_funcs; i++) {
-    perf = perf_test_joint_probs(joint_probs_func_registry.funcs[i], X);
-    cout << joint_probs_func_registry.func_names[i] << "," << perf << endl;
+    cout << joint_probs_func_registry.func_names[i] << ", ";
+  }
+  for (int i = 0; i < grad_desc_func_registry.num_funcs; i++) {
+    cout << grad_desc_func_registry.func_names[i];
+    if (i == grad_desc_func_registry.num_funcs-1) {
+      cout << endl;
+    } else {
+      cout << ", ";
+    }
   }
 
-  // Pick one joint_probs implementation to populate the variables.
-  auto joint_probs = joint_probs_func_registry.funcs[0];
-  for (int i = 0; i < grad_desc_func_registry.num_funcs; i++) {
-    perf = perf_test_grad_desc(grad_desc_func_registry.funcs[i], joint_probs, X,
-                               Y);
-    cout << grad_desc_func_registry.func_names[i] << "," << perf << endl;
+  for (int i=0; i<n_measurements; i++) {
+    for (int j=0; j<n_measurement_series; j++) {
+      cout << performances[i][j];
+      if (j == n_measurement_series-1) {
+        cout << endl;
+      } else {
+        cout << ", ";
+      }
+    }
   }
 
   return 0;
