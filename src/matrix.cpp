5ccc2040168b5df4d714d444d3627f705db16015
Manuel Nowack <mnowack@student.ethz.ch>
2021-06-15 15:03:30 +0200

    Fix compiler warnings in load_matrix and cover all edge cases

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -27,67 +28,88 @@ void destroy_tsne_variables(tsne_var_t &var) {
   free(var.D.data);
 }
 
+/*
+ * Return value:
+ *  0 Matrix was successfully loaded
+ *  1 Failed to open file at filepath
+ *  2 Failed to read next double in the file
+ *  3 Unexpected end of file
+ *  4 Unexpected number of doubles in a row 
+ *  5 Failed to allocate matrix
+ */
+int load_matrix_verbose(const char *filepath, Matrix *A) {
+  FILE *in_file = fopen(filepath, "r");
+  if (!in_file) {
+    return 1;
+  }
+
+  double val;
+  int delimiter, items_read;
+
+  // count columns
+  A->ncols = 0;
+  do {
+    items_read = fscanf(in_file, "%lf", &val);
+    if (items_read != 1) {
+      return 2;
+    }
+    A->ncols++;
+    delimiter = fgetc(in_file);
+    if (delimiter == EOF) {
+      return 3;
+    }
+  } while (delimiter != '\n');
+  A->nrows = 1;
+
+  // count rows
+  int counter = 0;
+  do {
+    items_read = fscanf(in_file, "%lf", &val);
+    if (items_read == EOF) {
+      break;
+    }
+    if (items_read != 1) {
+      return 2;
+    }
+    counter++;
+    delimiter = fgetc(in_file);
+    if (delimiter == '\n') {
+      // check if row contains as many elements as required
+      if (counter != A->ncols) {
+        return 4;
+      }
+      counter = 0;
+      A->nrows++;
+    }
+  } while (delimiter != EOF);
+
+  // load data
+  A->data = (double *)aligned_alloc(32, A->nrows * A->ncols * sizeof(double));
+  if (!A->data) {
+    return 5;
+  }
+  rewind(in_file);
+  int i = 0;
+  do {
+    items_read = fscanf(in_file, "%lf", &A->data[i++]);
+    if (items_read == EOF) {
+      break;
+    }
+    if (items_read != 1) {
+      return 2;
+    }
+    delimiter = fgetc(in_file);
+  } while (delimiter != EOF);
+  fclose(in_file);
+
+  return 0;
+}
 
 /*
  * Load data in text file at filepath into Matrix structure.
  */
 void load_matrix(const char *filepath, Matrix *A) {
-  // open file
-  FILE *in_file = fopen(filepath, "r");
-  if (in_file == NULL) {
-    throw std::runtime_error(
-        std::string("Could not load matrix from filepath ") + filepath);
-  }
-
-  // determine matrix dimension
-  A->nrows = 0;
-  A->ncols = 0;
-  A->data = NULL;
-
-  double val;
-  char character;
-
-  // count columns
-  do {
-    fscanf(in_file, "%lf", &val);
-    A->ncols++;
-    character = (char)fgetc(in_file);
-  } while (character != '\n' && character != EOF);
-  A->nrows++;
-
-  // count rows
-  int counter = 0;
-  do {
-    fscanf(in_file, "%lf", &val);
-    counter++;
-    character = (char)fgetc(in_file);
-    if (character == '\n') {
-      // check if row contains as many elements as required
-      if (counter != A->ncols) {
-        throw std::runtime_error(
-            std::string("Error: Invalid number of elements in row ") +
-            std::to_string(A->nrows) + ": " + std::to_string(counter) +
-            " instad of " + std::to_string(A->ncols));
-      }
-      counter = 0;
-      A->nrows++;
-    }
-  } while (character != EOF);
-
-  // load data
-  A->data = (double *)aligned_alloc(32, A->nrows * A->ncols * sizeof(double));
-  if (!A->data) {
-    throw std::runtime_error("Could not allocate memory to store matrix.");
-  }
-  rewind(in_file);
-  int i = 0;
-  do {
-    fscanf(in_file, "%lf", &A->data[i]);
-    i++;
-    character = (char)fgetc(in_file);
-  } while (character != EOF);
-  fclose(in_file);
-
+  assert(load_matrix_verbose(filepath, A) == 0);
   DEBUG("Matrix of dimension " << A->nrows << " x " << A->ncols << " loaded");
 }
 
