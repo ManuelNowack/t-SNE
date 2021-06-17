cbe38d63b994758bc68ea9bca446ce86e6ff9b9a
Kaishuo Zhang <kaiszhang@student.ethz.ch>
2021-06-17 13:27:44 +0200

    Valgrind cache sim parameters.

--- profiling/valgrind.sh
+++ profiling/valgrind.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in `seq 2 13`; do
-  valgrind --tool=callgrind --callgrind-out-file=analysis/tsne/callgrind.out.tsne.$i --simulate-cache=yes build/bin/profiling_tsne mnist10k_X.txt mnist10k_Y_init.txt $i $i
+  valgrind --tool=callgrind --callgrind-out-file=analysis/tsne/callgrind.out.tsne.$i --simulate-cache=yes --I1=32768,8,64 --D1=32768,8,64 --LL=3145728,12,64 build/bin/profiling_tsne mnist10k_X.txt mnist10k_Y_init.txt $i $i
 done
