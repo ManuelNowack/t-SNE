e5643c42babb0e194a6466434568a4b0ee2d6bdc
Manuel Nowack <mnowack@student.ethz.ch>
2021-06-19 13:00:33 +0200

    Configure sample size of benchmark

--- benchmark/benchmark_grad_desc.cpp
+++ benchmark/benchmark_grad_desc.cpp
@@ -52,11 +52,20 @@ double perf_grad_desc(grad_desc_func_t *f, const Matrix *Y, tsne_var_t *var) {
   return double(total_cycles) / REP;
 }
 
+int pow(int base, int exponent) {
+  int power = 1;
+  for (int i = 0; i < exponent; ++i)
+  {
+    power *= base;
+  }
+  return power;
+}
+
 
 int main(int argc, char const *argv[])
 {
-  if (argc < 3) {
-    printf("Usage: %s X_PCA Y_INIT\n", argv[0]);
+  if (argc < 3 || argc > 5) {
+    printf("Usage: %s X_PCA Y_INIT [min samples] [max samples]\n", argv[0]);
     return 1;
   }
 
@@ -66,83 +75,114 @@ int main(int argc, char const *argv[])
 
   const int n = Y.nrows;
   const int m = Y.ncols;
-  tsne_var_t var;
-  create_tsne_variables(var, n, m);
+  int n_min, n_max;
+  if (argc == 3) {
+    n_min = n;
+    n_max = n;
+  }
+  else if (argc == 4) {
+    n_min = atoi(argv[3]);
+    n_max = n_min;
+  }
+  else if (argc == 5) {
+    n_min = pow(2, atoi(argv[3]));
+    n_max = pow(2, atoi(argv[4]));
+  }
+  if (n_min < 1)
+  {
+    printf("%d samples are requested but at least one is required", n_min);
+    return 1;
+  }
+  if (n_max > n)
+  {
+    printf("%d samples are requested but only %d are available", n_max, n);
+    return 1;
+  }
 
-  joint_probs_baseline(&X, &var.P, &var.D);
+  for (int i = n_min; i <= n_max; i *= 2)
+  {
+    X.nrows = i;
+    Y.nrows = i;
+    tsne_var_t var;
+    create_tsne_variables(var, i, m);
 
-  double cycles;
-  cycles = perf_grad_desc(grad_desc_baseline, &Y, &var);
-  printf("grad_desc_baseline %e\n", cycles);
+    joint_probs_baseline(&X, &var.P, &var.D);
 
-  cycles = perf_grad_desc(_grad_desc_vec, &Y, &var);
-  printf("_grad_desc_vec %e\n", cycles);
+    printf("%d samples\n\n", i);
 
-  cycles = perf_grad_desc(_grad_desc_vec2, &Y, &var);
-  printf("_grad_desc_vec2 %e\n", cycles);
+    double cycles;
+    cycles = perf_grad_desc(grad_desc_baseline, &Y, &var);
+    printf("grad_desc_baseline %e\n", cycles);
 
-  cycles = perf_grad_desc(_grad_desc_vec3, &Y, &var);
-  printf("_grad_desc_vec3 %e\n", cycles);
+    cycles = perf_grad_desc(_grad_desc_vec, &Y, &var);
+    printf("_grad_desc_vec %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_baseline, &Y, &var);
-  printf("grad_desc_no_vars_baseline %e\n", cycles);
+    cycles = perf_grad_desc(_grad_desc_vec2, &Y, &var);
+    printf("_grad_desc_vec2 %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_tmp, &Y, &var);
-  printf("grad_desc_no_vars_tmp %e\n", cycles);
+    cycles = perf_grad_desc(_grad_desc_vec3, &Y, &var);
+    printf("_grad_desc_vec3 %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_D, &Y, &var);
-  printf("grad_desc_no_vars_D %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_baseline, &Y, &var);
+    printf("grad_desc_no_vars_baseline %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_Q, &Y, &var);
-  printf("grad_desc_no_vars_Q %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_tmp, &Y, &var);
+    printf("grad_desc_no_vars_tmp %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_Q_numerators, &Y, &var);
-  printf("grad_desc_no_vars_Q_numerators %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_D, &Y, &var);
+    printf("grad_desc_no_vars_D %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_scalar, &Y, &var);
-  printf("grad_desc_no_vars_scalar %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_Q, &Y, &var);
+    printf("grad_desc_no_vars_Q %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_no_if, &Y, &var);
-  printf("grad_desc_no_vars_no_if %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_Q_numerators, &Y, &var);
+    printf("grad_desc_no_vars_Q_numerators %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_grad, &Y, &var);
-  printf("grad_desc_no_vars_grad %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_scalar, &Y, &var);
+    printf("grad_desc_no_vars_scalar %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_means, &Y, &var);
-  printf("grad_desc_no_vars_means %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_no_if, &Y, &var);
+    printf("grad_desc_no_vars_no_if %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_unroll2, &Y, &var);
-  printf("grad_desc_no_vars_unroll2 %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_grad, &Y, &var);
+    printf("grad_desc_no_vars_grad %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_unroll4, &Y, &var);
-  printf("grad_desc_no_vars_unroll4 %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_means, &Y, &var);
+    printf("grad_desc_no_vars_means %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_unroll6, &Y, &var);
-  printf("grad_desc_no_vars_unroll6 %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_unroll2, &Y, &var);
+    printf("grad_desc_no_vars_unroll2 %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_unroll8, &Y, &var);
-  printf("grad_desc_no_vars_unroll8 %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_unroll4, &Y, &var);
+    printf("grad_desc_no_vars_unroll4 %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_fetch, &Y, &var);
-  printf("grad_desc_no_vars_fetch %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_unroll6, &Y, &var);
+    printf("grad_desc_no_vars_unroll6 %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_no_l, &Y, &var);
-  printf("grad_desc_no_vars_no_l %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_unroll8, &Y, &var);
+    printf("grad_desc_no_vars_unroll8 %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_unroll, &Y, &var);
-  printf("grad_desc_no_vars_unroll %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_fetch, &Y, &var);
+    printf("grad_desc_no_vars_fetch %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_vector, &Y, &var);
-  printf("grad_desc_no_vars_vector %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_no_l, &Y, &var);
+    printf("grad_desc_no_vars_no_l %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_vector_acc, &Y, &var);
-  printf("grad_desc_no_vars_vector_acc %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_unroll, &Y, &var);
+    printf("grad_desc_no_vars_unroll %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_vector_inner, &Y, &var);
-  printf("grad_desc_no_vars_vector_inner %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_vector, &Y, &var);
+    printf("grad_desc_no_vars_vector %e\n", cycles);
 
-  cycles = perf_grad_desc(grad_desc_no_vars_vector_unroll2, &Y, &var);
-  printf("grad_desc_no_vars_vector_unroll2 %e\n", cycles);
+    cycles = perf_grad_desc(grad_desc_no_vars_vector_acc, &Y, &var);
+    printf("grad_desc_no_vars_vector_acc %e\n", cycles);
 
-  destroy_tsne_variables(var);
+    cycles = perf_grad_desc(grad_desc_no_vars_vector_inner, &Y, &var);
+    printf("grad_desc_no_vars_vector_inner %e\n", cycles);
+
+    cycles = perf_grad_desc(grad_desc_no_vars_vector_unroll2, &Y, &var);
+    printf("grad_desc_no_vars_vector_unroll2 %e\n", cycles);
+
+    destroy_tsne_variables(var);
+  }
 }
